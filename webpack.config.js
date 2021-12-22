f80272c5fe55dbeaa76d120c2e90e21c0987e688
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2021-12-22 17:07:55 +0900

    Apply Prettier

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,30 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	mode: "development",
-	entry: "./src/index.tsx",
-	module: {
-		rules: [
-			{
-				test: /\.(ts|tsx)$/,
-				use: ["ts-loader"],
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader"],
-			}
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./src/index.html",
-		}),
-	],
-	devServer: {
-		static: {
-			directory: path.join(__dirname, "dist"),
-		},
-		hot: true,
-	},
+  mode: "development",
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: ["ts-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    hot: true,
+  },
 };
